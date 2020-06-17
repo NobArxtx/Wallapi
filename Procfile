b40b863af9f82bb09b14284b43db89eadbb25bab
NobArxtx <andratatti@gmail.com>
2020-06-17 08:34:21 +0530

    d

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Apiwall.wsgi --log-file -
---
+++ api/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bs4 import BeautifulSoup as soup
+import requests
+import html
+from random import randint,choice
+# Create your views here.
+import json 
+def walld(strin: str):
+    if len(strin.split()) > 1:
+        strin = '+'.join(strin.split())
+    url = 'https://wall.alphacoders.com/search.php?search='
+    none_got = ['https://wall.alphacoders.com/finding_wallpapers.php']
+    none_got.append("https://wall.alphacoders.com/search-no-results.php")
+    page_link = 'https://wall.alphacoders.com/search.php?search={}&page={}'
+    resp = requests.get(f'{url}{strin}')
+    if resp.url in none_got:
+        return False
+    if 'by_category.php' in resp.url:
+        page_link = str(resp.url).replace('&amp;', '') + '&page={}'
+        check_link = True
+    else:
+        check_link = False
+    resp = soup(resp.content, 'html.parser')
+    try:
+        page_num = resp.find('div', {'class': 'visible-xs'})
+        page_num = page_num.find('input', {'class': 'form-control'})
+        page_num = int(page_num['placeholder'].split(' ')[-1])
+    except Exception:
+        page_num = 1
+    n = randint(1, page_num)
+    if page_num != 1:
+        if check_link:
+            resp = requests.get(page_link.format(n))
+        else:
+            resp = requests.get(page_link.format(strin, n))
+        resp = soup(resp.content, 'html.parser')
+    a_s = resp.find_all('a')
+    list_a_s = []
+    tit_links = []
+    r = ['thumb', '350', 'img', 'big.php?i', 'data-src', 'title']
+    for a_tag in a_s:
+        if all(d in str(a_tag) for d in r):
+            list_a_s.append(a_tag)
+    try:
+        for df in list_a_s:
+            imgi = df.find('img')
+            li = str(imgi['data-src']).replace('thumb-350-', '')
+            titl = str(df['title']).replace('|', '')
+            titl = titl.replace('  ', '')
+            titl = titl.replace('Image', '')
+            titl = titl.replace('HD', '')
+            titl = titl.replace('Wallpaper', '')
+            titl = titl.replace('Background', '')
+            if li is not None and titl is not None:
+                titl =  titl.strip()
+                p = {"Link": li, "title":titl}
+                tit_links.append(p)
+    except Exception:
+        pass
+    del list_a_s
+    return tit_links
+
+def wall_find(request):
+    if request.method == 'GET':
+        strin = request.GET['search']
+        inf = walld(strin)
+        
+        if inf:
+            return HttpResponse(json.dumps({'search':strin,'Links':inf}))
+        else:
+            return HttpResponse(json.dumps({'search':inf}))

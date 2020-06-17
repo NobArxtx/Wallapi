6aed077620508ced36f6425d50b38471cc4bc309
NobArxtx <andratatti@gmail.com>
2020-06-17 08:48:41 +0530

    d

--- Apiwall/settings.py
+++ Apiwall/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.

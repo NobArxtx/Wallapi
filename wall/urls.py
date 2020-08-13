11ec87e69168ba6502a867f4601114fd4e2bae43
NobArxtx <54905770+NobArxtx@users.noreply.github.com>
2020-08-14 00:21:25 +0530

    Update urls.py

--- wall/urls.py
+++ wall/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , re_path
 from . import views
 
 urlpatterns = [
-    path('search/', views.wall_find),
+    re_path(r'^search/(?P<q>\w)/$', views.wall_find),
     path('', views.red_to_index),
     
 ]
